46a4228c642f632fc0118a972fa54a7e592a9f9b
davidenovarini <116503113+davidenovarini@users.noreply.github.com>
2022-11-23 14:55:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+
+    int n,a=1,b=1,c=0;
+
+    cin>>n;
+
+    if (n>=2)
+    {
+        cout<<a<<"\n"<<b<<"\n";
+
+        for(int i=2;i<n;i++){
+
+            c=a+b;
+            cout << c << std::endl;
+            a=b;
+            b=c;
+
+
+        }
+
+
+
+
+    }
+    else
+        cout << "errore" << endl;
+
+
+
+
+
+    return 0;
 }
